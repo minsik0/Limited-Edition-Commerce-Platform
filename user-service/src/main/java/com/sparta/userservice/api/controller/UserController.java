7f6d0a9c06128054eb579cad63dd69737f84d598
minsik0 <qwew1212@naver.com>
2026-01-29 19:00:26 +0900

    fix: User Controller principal 적용

--- user-service/src/main/java/com/sparta/userservice/api/controller/UserController.java
+++ user-service/src/main/java/com/sparta/userservice/api/controller/UserController.java
@@ -7,12 +7,14 @@ import com.sparta.userservice.application.dto.request.UserLoginRequest;
 import com.sparta.userservice.application.dto.request.UserSignupRequest;
 import com.sparta.userservice.application.dto.request.UserUpdateRequest;
 import com.sparta.userservice.application.service.UserService;
+import com.sparta.userservice.infrastructure.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +39,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<Page<UserListResponse>> getUsers(@PageableDefault(size = 30) Pageable pageable) {
         return ResponseEntity.ok(userService.getUsers(pageable));
@@ -44,22 +47,25 @@ public class UserController {
 
     @GetMapping("/me")
     public ResponseEntity<UserInfoResponse> getMyInfo(Authentication authentication) {
-        UUID userId = (UUID) authentication.getPrincipal();
-        return ResponseEntity.ok(userService.getMyInfo(userId));
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+
+        return ResponseEntity.ok(userService.getMyInfo(principal.getUserId()));
     }
 
     @PatchMapping("/me")
     public ResponseEntity<ApiResponse<UserUpdateResponse>> updateMyInfo(Authentication authentication,
                                                                         @RequestBody UserUpdateRequest request) {
-        UUID userId = (UUID) authentication.getPrincipal();
-        UserUpdateResponse response = userService.updateMyInfo(userId, request);
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+
+        UserUpdateResponse response = userService.updateMyInfo(principal.getUserId(), request);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     @DeleteMapping("/me")
     public ResponseEntity<ApiResponse<UserDeleteResponse>> deleteMyInfo(Authentication authentication) {
-        UUID userId = (UUID) authentication.getPrincipal();
-        UserDeleteResponse response = userService.deleteMyInfo(userId);
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+
+        UserDeleteResponse response = userService.deleteMyInfo(principal.getUserId());
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
