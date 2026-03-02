441ba3938ad70586cdd59f28c9c4012813451149
minsik0 <qwew1212@naver.com>
2026-03-03 02:53:27 +0900

    update: Product index.sql 추가

--- product-service/src/main/resources/db/index.sql
+++ product-service/src/main/resources/db/index.sql
@@ -1,5 +1,5 @@
 CREATE INDEX idx_products_deleted_at
     ON products (deleted_at);
 
-CREATE INDEX idx_product_options_product_deleted
-    ON product_options (product_id, deleted_at);
+CREATE INDEX idx_product_options_product_deleted_stock
+    ON product_options (product_id, deleted_at, remain_stock);
