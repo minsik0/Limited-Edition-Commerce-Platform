b6c25cd886b65cb3354b7ad4ef757a88a2df8f2a
minsik0 <qwew1212@naver.com>
2026-03-19 20:57:58 +0900

    fix: Product Redis CacheManager 생성 코드수정

--- product-service/src/main/java/com/sparta/productservice/infrastructure/cache/RedisConfig.java
+++ product-service/src/main/java/com/sparta/productservice/infrastructure/cache/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.sparta.productservice.infrastructure.cache;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,22 +27,19 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory,
-                                          @org.springframework.beans.factory.annotation.Qualifier("redisObjectMapper")
-                                          com.fasterxml.jackson.databind.ObjectMapper objectMapper) {
+                                          @Qualifier("redisObjectMapper") ObjectMapper objectMapper) {
 
+        // 1. 캐시 세부 설정
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-
                 .entryTtl(Duration.ofDays(7))
-
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
-
                 .disableCachingNullValues();
 
-        return RedisCacheManager.builder()
+        // 2. RedisCacheManager 생성
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(connectionFactory)
                 .cacheDefaults(config)
                 .build();
-
     }
 }
