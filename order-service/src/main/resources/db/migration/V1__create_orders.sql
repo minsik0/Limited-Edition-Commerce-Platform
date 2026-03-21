3a889b018e7b3699291891b2a0be3dca0aa9e192
minsik0 <qwew1212@naver.com>
2026-03-21 21:48:09 +0900

    fix: yml, Dockerfile, sql 수정

---
+++ order-service/src/main/resources/db/migration/V1__create_orders.sql
@@ -0,0 +1,24 @@
+-- orders
+CREATE TABLE orders (
+                        order_id UUID PRIMARY KEY,
+                        user_id UUID NOT NULL,
+                        status VARCHAR(50) NOT NULL,
+                        total_price INT NOT NULL,
+                        created_at TIMESTAMP,
+                        updated_at TIMESTAMP,
+                        deleted_at TIMESTAMP
+);
+
+-- order_items
+CREATE TABLE order_items (
+                             order_item_id UUID PRIMARY KEY,
+                             order_id UUID NOT NULL,
+                             product_id UUID NOT NULL,
+                             product_name VARCHAR(255) NOT NULL,
+                             option_id UUID NOT NULL,
+                             option_name VARCHAR(255) NOT NULL,
+                             price INT NOT NULL,
+                             quantity INT NOT NULL,
+                             CONSTRAINT fk_order_items_order
+                                 FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
--- product-service/src/main/resources/db/migration/V3__add_indexes.sql
+++ product-service/src/main/resources/db/migration/V3__add_indexes.sql
@@ -1,5 +1,2 @@
 CREATE INDEX idx_products_deleted_openat
     ON products (deleted_at, open_at DESC);
-
-CREATE INDEX idx_product_options_product_deleted_stock
-    ON product_options (product_id, deleted_at, remain_stock);
