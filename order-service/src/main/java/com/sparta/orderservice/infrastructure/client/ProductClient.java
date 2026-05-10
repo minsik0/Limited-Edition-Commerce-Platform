e6812bb8e2337858364f2abc1440d2a971dfb0ea
minsik0 <qwew1212@naver.com>
2026-05-10 18:04:05 +0900

    fix: Order feignClient 수정

--- order-service/src/main/java/com/sparta/orderservice/infrastructure/client/ProductClient.java
+++ order-service/src/main/java/com/sparta/orderservice/infrastructure/client/ProductClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
-@FeignClient(name = "product-service", url = "${product.service.url}")
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
     @GetMapping("/internal/products/{productId}/options/{optionId}")
