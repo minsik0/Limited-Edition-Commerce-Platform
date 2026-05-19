1f81d831a1e84b74aff2d67b4471219284b22e29
minsik0 <qwew1212@naver.com>
2026-05-19 22:21:09 +0900

    fix: Gateway TraceLoggingFilter RequestLoggingFilter 병합

---
+++ gateway/src/main/java/com/sparta/gateway/filter/LoggingFilter.java
@@ -0,0 +1,74 @@
+package com.sparta.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class LoggingFilter implements GlobalFilter {
+
+    private static final String TRACE_ID = "X-Trace-Id";
+
+    @Override
+    public Mono<Void> filter(
+            ServerWebExchange exchange,
+            GatewayFilterChain chain
+    ) {
+
+        String traceId = exchange.getRequest()
+                .getHeaders()
+                .getFirst(TRACE_ID);
+
+        if (traceId == null) {
+            traceId = UUID.randomUUID().toString();
+        }
+
+        final String finalTraceId = traceId;
+        final long start = System.currentTimeMillis();
+
+        ServerHttpRequest mutatedRequest = exchange.getRequest()
+                .mutate()
+                .header(TRACE_ID, traceId)
+                .build();
+
+        ServerWebExchange mutatedExchange = exchange
+                .mutate()
+                .request(mutatedRequest)
+                .build();
+
+        String method =
+                String.valueOf(mutatedRequest.getMethod());
+
+        String path =
+                mutatedRequest.getURI().getPath();
+
+        log.info(
+                "[TRACE_ID={}] {} {} START",
+                traceId,
+                method,
+                path
+        );
+
+        return chain.filter(mutatedExchange)
+                .doFinally(signalType -> {
+
+                    long duration =
+                            System.currentTimeMillis() - start;
+
+                    log.info(
+                            "[TRACE_ID={}] {} {} END {}ms",
+                            finalTraceId,
+                            method,
+                            path,
+                            duration
+                    );
+                });
+    }
+}
